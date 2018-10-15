3a45eb8b71c7468cc400785778ec6f3aadfa4583
aoife kavanagh <nyhankaa@tcd.ie>
2018-10-15 14:03:07 +0000

    testing filter function

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = 'brewjar-app'
-app.config['MONGO_URI'] = 'mongodb://admin:99Prism@ds225382.mlab.com:25382/brewjar_app'
+app.config['MONGO_DBNAME'] = 'brewjar'
+app.config['MONGO_URI'] = 'mongodb://admin:99Prism@ds149732.mlab.com:49732/brewjar'
 
 mongo = PyMongo(app)
 bcrypt = Bcrypt(app)
@@ -44,33 +44,28 @@ def choose_a_brew():
         
 @app.route('/brew-results', methods=['GET','POST'])
 def brew_results():
-    brews = mongo.db.brew
+    all_brews = mongo.db.brew.category.find()
+    brew_choices = []
     
-    filterby = []
+    brew_types = request.form.get('brew-type')
+    brew_choices.append(brew_types)
     
-    brewtype= request.form.get('brew-type')
-    stufffree = request.form.get('stuff-free')
-    level = request.form.get('level')
-    
-    filterby.append(brewtype)
-    filterby.append(stufffree)
-    filterby.append(level)
-    
-    def gg(filtermatch):
-    
-        if (filtermatch in filterby):
-            return True;
+    def filtermatch(brew_choices):
+        if (brew_choices in all_brews):
+            return True
         else:
             return False
+        
+    filter_results = filter(filtermatch, brew_types)
+        
+    # stufffree = request.form.get('stuff-free')
+    # level = request.form.get('level')
+   
+    return render_template('brewresults.html', filter_results=filter_results)
     
-    brews2 = brews.find()    
-    brewresults = filter(gg, brews2)
-            
-    return render_template('brewresults.html', brewresults=brewresults)
-    
-@app.route('/success')
-def success():
-    return render_template('success.html')
+# @app.route('/success')
+# def success():
+#     return render_template('success.html')
 
 @app.route('/add-profile')
 def add_profile():
